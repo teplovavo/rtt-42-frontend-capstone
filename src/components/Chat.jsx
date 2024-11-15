d94172f2944c716a7c63553ce2897ba5f77e036d
Veronika Teplova <teplovavo@gmail.com>
2024-11-15 11:02:23 -0700

    Chat function added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,66 @@
+import { useState, useEffect, useRef } from "react";
+
 export default function Chat() {
-    return (
+  const [userInput, setUserInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();      
+  }, []);
+
+  const handleChange = (e) => {
+    const text = e.target.value.trim();
+    console.log(text);
+    setUserInput(text);
+  };
+
+
+const handleSubmit = (e) => {
+    try {
+      e.preventDefault();
+      console.log('Data: ${userInput}');
+      const message = {
+        role: "user",
+        content: userInput
+      }
+      setMessages([...messages, message]);
+
+      setUserInput('');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+        {/* {MESSAGES} */}
         <div>
-            <h1>Chat</h1>
+            {messages.map((m, i) =>
+                <div key={i}>
+                    <p>{m.role.toUpperCase()}</p>
+                    <p>{m.content}</p>
+                </div>
+            )}
         </div>
-    );
+
+
+       {/* {FORM} */}
+      <form onSubmit={handleSubmit}>
+        <label >
+          <input
+            type="text"
+            name="userInput"
+            value={userInput}
+            ref={inputRef}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button>Send</button>
+
+      </form>
+    </div>
+  );
 }
--- src/pages/ChatsPage.jsx
+++ src/pages/ChatsPage.jsx
@@ -1,12 +1,14 @@
-
+import Chat from "../components/Chat";
 
 
 
 export default function ChatsPage() {
     return (
         <main>
-            <h1>Chats</h1>
-            
+            <h1>Chats page</h1>
+
+            <Chat />
+
         </main>
-        )
+    );
 }
